6ecd0f2f40583c232df298ff40390743b5f705a9
Zhe WANG <zhe.wang@etu.sorbonne-universite.fr>
2022-11-15 11:11:12 +0100

    initial

---
+++ fic1.pl
@@ -0,0 +1,32 @@
+equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(auteur,and(personne,some(aEcrit,livre))).
+equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite
+,livre)))).
+equiv(parent,and(personne,some(aEnfant,anything))).
+cnamea(personne).
+cnamea(livre).
+cnamea(objet).
+cnamea(sculpture).
+cnamea(anything).
+cnamea(nothing).
+cnamena(auteur).
+cnamena(editeur).
+cnamena(sculpteur).
+cnamena(parent).
+iname(michelAnge).
+iname(david).
+iname(sonnets).
+iname(vinci).
+iname(joconde).
+rname(aCree).
+rname(aEcrit).
+rname(aEdite).
+rname(aEnfant).
+inst(michelAnge,personne).
+inst(david,sculpture).
+inst(sonnets,livre).
+inst(vinci,personne).
+inst(joconde,objet).
+instR(michelAnge, david, aCree).
+instR(michelAnge, sonnets, aEcrit).
+instR(vinci, joconde, aCree).
